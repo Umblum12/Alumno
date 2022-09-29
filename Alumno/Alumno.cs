17c21889d8a6c77f1a64c0666a299f04c1bdd29e
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:47:50 -0500

    Initial commit

---
+++ Alumno/Alumno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumno
+{
+    internal class Alumno
+    {
+        //Propiedades
+        private string Nombre;
+        private int Edad;
+        //Constructor
+        public Alumno()
+        {
+            Nombre = "Abelardo Cruz Leos";
+            Edad = 10;
+        }
+        //Metodos
+        public void Imprimir_area_Triangulo()
+        {
+            Console.WriteLine("El Nombre del Alumno es: " + Nombre);
+        }
+        public void Imprimir_area_Trapecio()
+        {
+            Console.WriteLine("La edad del Alumno es: " + Edad);
+        }
+    }
+}
